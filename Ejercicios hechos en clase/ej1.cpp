96edb41419ef94a9ac65de1469001d35d122b5c7
sebastian-cipriano-damian-2019630004 <85646953+sebastian-cipriano-damian-2019630004@users.noreply.github.com>
2021-06-24 16:52:10 -0500

    Evidencias de Ejercicios
    Se agregaron las evidencias de ejercicios hechos en clase

---
+++ Ejercicios hechos en clase/ej1.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void A(const string&, int&);
+void B(const string&, int&);
+
+void error(){
+    cout << "No le sabes"<< endl;
+    exit(1);
+}
+
+void consume(char x, const string s, int id){
+    if(s[id] == x){
+        id++;
+    }
+    else{
+        error();
+    }
+}
+
+void Grammar(string s){
+    int id = 0;
+    A(s, id);
+    cout<<"Si pertenece"<<endl;
+}
+void A(const string s, int id){
+    consume('a', s, id); 
+	B(s, id); 
+	consume('a', s, id);
+}
+
+void B(const string s, int id){
+    switch(s[id]){
+        case 'b':
+            consume('b', s, id); A(s, id); consume('b', s, id);
+            break;
+        case 'a':
+            consume('a', s, id); 
+            break;
+        default:
+            error();
+    }
+}
+
+int main(){
+    while(true){
+        string s; 
+		cin>>s;
+        Grammar(s);
+    }
+	return 0;
+}
